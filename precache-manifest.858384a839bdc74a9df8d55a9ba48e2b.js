dedd800c3edd42fec3d306f61c29ee967e6633be
Joshua Gomora <joshuag@platypuslocal.com>
2020-03-03 23:33:51 +0800

    Updates

---
+++ precache-manifest.858384a839bdc74a9df8d55a9ba48e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038b61f9e0510043d74628cd82ae196f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b88c3456c5bf9256b0a2",
+    "url": "/monsters-rolodex/static/css/main.8e2d09c7.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88c3456c5bf9256b0a2",
+    "url": "/monsters-rolodex/static/js/main.d92ca203.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
